037e5b5cfda8a44ee92fbfe08a1e354f37b6851a
Mikhail-Kushnerev <mikushnerev@yandex.ru>
2022-04-16 21:09:36 +0300

    requirements.txt

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -12,12 +12,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'abcdefg12345'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
